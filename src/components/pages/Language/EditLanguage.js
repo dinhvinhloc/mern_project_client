b3301f24437e6f150e1e4382944239be65bac90e
dinhvinhloc <39650247+dinhvinhloc@users.noreply.github.com>
2021-06-21 00:20:59 -0400

    Add Language detail pages

--- src/components/pages/Language/EditLanguage.js
+++ src/components/pages/Language/EditLanguage.js
@@ -1,7 +1,97 @@
 import React from 'react';
+import Breadcrumbs from '../../layouts/Breadcrumbs';
+import { Button, Card, Form, Col, Alert } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
 
-const EditLanguage = () => {
-  return <div>Edit Language page</div>;
-};
+
+const Levels = ['CLB 4','CLB 5','CLB 6','CLB 7','CLB 8','CLB 9','CLB 10',];
+
+const breadcrumbLinks = [
+  {
+    label: 'Home',
+    path: '/'
+  },
+  {
+    label: 'Language',
+    path: '/language',
+  },
+  {
+    label: 'Update language',
+    active: true
+  }
+];
+
+class EditLanguage extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      id: '',
+      language: '',
+      level: '',
+      messageVariant: 'success',
+      hasMessage: false,
+      messageInfo: ''
+    };
+  }
+
+  componentDidMount() {
+    
+  }
+
+  saveHandler = (e) => {
+    
+  }
+
+  handleValueChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  render() {
+    return (
+      <div>
+        <Breadcrumbs links={breadcrumbLinks} />
+        {  
+          this.state.hasMessage ? <Alert variant={this.state.messageVariant}>{this.state.messageInfo}</Alert> : ''
+        }
+        
+        <Card
+          bg='light'
+          text='dark'
+        >
+          <Card.Header>Update Language</Card.Header>
+          <Card.Body>
+            <Form>
+              <Form.Group>
+                <Form.Row>
+                  <Col>
+                    <Form.Label>Language</Form.Label>
+                    <Form.Control onChange={this.handleValueChange} value={this.state.language} name='language' size='sm' type="text" placeholder="Enter language" />
+                  </Col>
+                  <Col>
+                    <Form.Label>level</Form.Label>
+                    <Form.Control onChange={this.handleValueChange} value={this.state.level} name='level' size='sm' as="select">
+                      <option value='' key='-1'>--- Please select level ---</option>
+                      {
+                        Levels.map((value, index) => (
+                          <option value={value} key={index}>{value}</option>
+                        ))
+                      } 
+                    </Form.Control>
+                  </Col>
+                </Form.Row>
+              </Form.Group>
+            </Form>
+          </Card.Body>
+          <Card.Footer>
+            <Button size='sm' onClick={this.saveHandler} variant="success" type="submit" className='float-right'>Save</Button>
+            <NavLink exact to='/language' className='btn btn-outline-secondary btn-sm float-left'>Back to Language</NavLink>
+          </Card.Footer>
+        </Card>
+      </div>
+    );
+  }
+
+}
 
 export default EditLanguage;
--- src/components/pages/Language/Language.js
+++ src/components/pages/Language/Language.js
@@ -1,7 +1,118 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Breadcrumbs from '../../layouts/Breadcrumbs';
+import { Table, Button, Card, Form, Col } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
+import { FaPenSquare, FaTrash } from 'react-icons/fa';
 
-const Language = () => {
-  return <div>Language page</div>;
-};
+
+
+const breadcrumbLinks = [
+  {
+    label: 'Home',
+    path: '/'
+  },
+  {
+    label: 'Language',
+    path: '/language',
+    active: true
+  }
+];
+
+class Language extends Component {
+
+  state = {
+    languages: [
+      {
+        "id": "1",
+        "language": "English",
+        "level": "CLB7",
+      },
+      {
+        "id": "2",
+        "language": "French",
+        "level": "CLB5",
+      },
+    ],
+    searchKeyword: ''
+  }
+
+  onLoadData() {
+
+  }
+
+  componentDidMount() {
+  }
+
+  handleValueChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  onDeleteHandler = (id) => {
+
+  }
+
+  render() {
+    return (
+      <div>
+        <Breadcrumbs links={breadcrumbLinks} />
+        <Card
+          bg='light'
+          text='dark'
+        >
+          <Card.Header>Languages</Card.Header>
+          <Card.Body>
+            <Form className='float-left'>
+              <Form.Row className="align-items-center">
+                <Col xs="auto">
+                  <Form.Label srOnly>Language</Form.Label>
+                  <Form.Control
+                    size="sm"
+                    className="mb-4"
+                    id="inlineFormInput"
+                    placeholder="Language"
+                    name="searchKeyword"
+                    onChange={this.handleValueChange}
+                  />
+                </Col>
+                <Col xs="auto">
+                  <Button size="sm" type="submit" className="mb-4" variant='info'>Search</Button>
+                </Col>
+              </Form.Row>
+            </Form>
+            {/* <NavLink exact to='/language/add' className='btn btn-sm btn-outline-secondary float-right'>New Language</NavLink> */}
+            <NavLink exact to='/language/add' className='myButton'>Add Language</NavLink>
+            <Table bordered hover>
+              <thead>
+                <tr>
+                  <th>#</th>
+                  <th>Language</th>
+                  <th>Level</th>
+                  <th>Actions</th>
+                </tr>
+              </thead>
+              <tbody>
+                {
+                  this.state.languages.map((lang, index) =>
+                    lang.language.toLowerCase().includes(this.state.searchKeyword.toLowerCase()) ?
+                      <tr key={index}>
+                        <td>{index + 1}</td>
+                        <td>{lang.language}</td>
+                        <td>{lang.level}</td>
+                        <td className='text-center'>
+                          <NavLink exact to={'/language/edit/' + lang.id} className='mr-3'><FaPenSquare className='text-warning' /></NavLink>
+                          <NavLink exact to='#' className='mr-3'><FaTrash className='text-danger' onClick={() => this.onDeleteHandler(lang._id)} /></NavLink>
+                        </td>
+                      </tr> : ''
+                  )
+                }
+              </tbody>
+            </Table>
+          </Card.Body>
+        </Card>
+      </div>
+    );
+  }
+
+}
 
 export default Language;
