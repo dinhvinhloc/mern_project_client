e9cbe9f93cce6d0600ed4c165f1a53cccc9fc286
dinhvinhloc <39650247+dinhvinhloc@users.noreply.github.com>
2021-06-21 22:37:05 -0400

    adding for Home page

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import {NavLink} from 'react-router-dom';
+import HomeCarousel from './HomeCarousel';
 
-const Home = () => {
-  return <div>Home page</div>;
-};
+function Home() {
+    return (
+        <div className="bodyLayout">
+              <h3>Home</h3>
+              <HomeCarousel/>
+        </div>
+    );
+}
 
 export default Home;
