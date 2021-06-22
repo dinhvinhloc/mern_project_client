3ad357d4af9c61ee8d60d90bde3c6d0fa053359b
SubhamTandel <subhamtandel@gmail.com>
2021-06-22 02:05:58 -0400

    Made changes to the Education And Experience Pages

--- src/components/pages/Education/EditEducation.js
+++ src/components/pages/Education/EditEducation.js
@@ -1,30 +1,89 @@
-import React from 'react';
+import React, {Component} from 'react';
+import Breadcrumbs from '../../layouts/Breadcrumbs';
+import { Button, Card, Form, Alert } from 'react-bootstrap';
 import {NavLink} from 'react-router-dom';
 
-const EditEducation = () => {
-  return <div className="bodyLayout">
-  <h3>Edit Education page</h3>
-  <table className='skillTable'>
-  <tr>
-      <td className='skillTableTd'>Eduction Start Year</td>
-      <td><input type="text" className="skillTextfield" name="edStartYear" placeholder="2021"/></td>
-    </tr>
-    <tr>
-      <td className='skillTableTd'>Eduction End Year</td>
-      <td><input type="text" className="skillTextfield" name="edEndYear" placeholder="2021"/></td>
-    </tr>
-    <tr>
-      <td className='skillTableTd'>Institute Name</td>
-      <td><input type="text" className="skillTextfield" name="inName" placeholder="Humber College"/></td>
-    </tr>
-    <tr>
-      <td className='skillTableTd'>Course Name</td>
-      <td><input type="text" className="skillTextfield" name="csName" placeholder="ITS"/></td>
-    </tr>
-  </table>
-  <NavLink to="/education" className="myButton">Save</NavLink>
-  <NavLink to="/education" className="myButton">Cancel</NavLink>
-</div>;
+
+const breadcrumbLinks = [
+  {
+    label: 'Home',
+    path: '/'
+  },
+  {
+    label: 'Education',
+    path: '/education',
+  },
+  {
+    label: 'Edit Education',
+    path: '/education/edit',
+    active: true
+  }
+];
+
+
+
+class EditEducation extends Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      messageVariant: 'danger',
+      hasMessage: false,
+      messageInfo: '',
+    };
+  }
+
+  saveHandler = (e) => {
+
+
+  }
+
+  handleValueChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  render() {
+    return (
+      <div>
+        <Breadcrumbs links={breadcrumbLinks} />
+        {  
+          this.state.hasMessage ? <Alert variant={this.state.messageVariant}>{this.state.messageInfo}</Alert> : ''
+        }
+        <Card
+          bg='light'
+          text='dark'
+        >
+          <Card.Header>Edit Education</Card.Header>
+          <Card.Body>
+            <Form>
+              <Form.Group>
+                <Form.Label>Eduction Start Year</Form.Label>
+                <Form.Control size='sm' type="text" name='syear' placeholder="2021" onChange={this.handleValueChange} />
+              </Form.Group>
+              <Form.Group>
+                <Form.Label>Eduction End Year</Form.Label>
+                <Form.Control size='sm' type="text" name='eyear' placeholder="2021" onChange={this.handleValueChange} />
+              </Form.Group>
+              <Form.Group>
+                <Form.Label>Institue Name</Form.Label>
+                <Form.Control size='sm' type="text" name='instName' placeholder="Humber College" onChange={this.handleValueChange} />
+              </Form.Group>
+              <Form.Group>
+                <Form.Label>Course Name</Form.Label>
+                <Form.Control size='sm' type="text" name='cName' placeholder="ITS" onChange={this.handleValueChange} />
+              </Form.Group>
+            </Form>
+          </Card.Body>
+          <Card.Footer>
+            <Button size='sm' onClick={this.saveHandler} variant="success" type="submit" className='float-right'>Save</Button>
+            <NavLink exact to='/education' className='btn btn-outline-secondary btn-sm float-left'>Back to Education</NavLink>
+          </Card.Footer>
+        </Card>
+      </div>
+    );
+  }
+
 };
 
 export default EditEducation;
--- src/components/pages/Education/Education.js
+++ src/components/pages/Education/Education.js
@@ -1,24 +1,129 @@
+import React, {Component} from 'react';
+import Breadcrumbs from '../../layouts/Breadcrumbs';
+import { Table, Button, Card, Form, Col } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
+import { FaPenSquare, FaTrash } from 'react-icons/fa';
 
-import {NavLink} from 'react-router-dom';
+const breadcrumbLinks = [
+  {
+    label: 'Home',
+    path: '/'
+  },
+  {
+    label: 'Education',
+    path: '/education',
+    active: true
+  }
+];
 
-const Education = () => {
+  
 
-  return <div className="bodyLayout">
-              <h3>Education Page</h3>
-              <table border='1' className="skillTable">
+
+class Education extends Component {
+
+  state = {
+    Education: [
+      {
+        "id": "1",
+        "syear": "2020",
+        "eyear": "2021",
+        "iname": "Humber College",
+        "cname": "Information Technology Solutions",
+      },
+      {
+        "id": "2",
+        "syear": "2015",
+        "eyear": "2019",
+        "iname": "Laxmi Institute of Technology",
+        "cname": "Computer Science Engineering",
+      },
+      
+    ],
+    searchKeyword: ''
+    
+  }
+
+  onLoadData() {
+
+  }
+
+  componentDidMount() {
+    this.onLoadData();
+  }
+
+  handleValueChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  onDeleteHandler = (id) => {
+
+  }
+
+  render(){
+  return (<div className="bodyLayout">
+
+<Breadcrumbs links={breadcrumbLinks} />
+        <Card
+          bg='light'
+          text='dark'
+        >
+          <Card.Header>Education</Card.Header>
+          <Card.Body>
+          <Form className='float-left'>
+              <Form.Row className="align-items-center">
+                <Col xs="auto">
+                  {/* <Form.Label srOnly>Skill</Form.Label> */}
+                  <Form.Control
+                    size="sm"
+                    className="mb-4"
+                    id="inlineFormInput"
+                    placeholder="Course Name"
+                    name="searchKeyword"
+                    onChange={this.handleValueChange}
+                  />
+                </Col>
+                <Col xs="auto">
+                  <Button size="sm" type="submit" className="mb-4" variant='info'>Search</Button>
+                </Col>
+              </Form.Row>
+            </Form>
+            
+            
+               <NavLink exact to='/education/add' className='myButton'>Add Education</NavLink>
+            <Table bordered hover>
+              <thead>
                 <tr>
-                  <th className='skillsTablePad'>Start Year</th>
-                  <th className='skillsTablePad'>End Year</th>
-                  <th className='skillsTablePad'>Institute Name</th>
-                  <th className='skillsTablePad'>Course Name</th>
-                  <th className='skillsTablePad'> Action </th>
+                  <th>#</th>
+                  <th>Start Year</th>
+                  <th>End Year</th>
+                  <th>Institute Name</th>
+                  <th>Course Name</th>
+                  <th>Actions</th>
                 </tr>
-              </table>
-              <NavLink to='/education/add' className='myButton'>Add</NavLink>
-              <NavLink to='/education/edit/:id' className='myButton'>Edit</NavLink>
-              <NavLink to='/education/delete/:id' className='myButton'>Delete</NavLink>
-              <NavLink to='/Experience' className='myButton'>Next</NavLink>
-        </div>;
+              </thead>
+              <tbody>
+                {
+                  this.state.Education.map((education, index) =>
+                    education.cname.toLowerCase().includes(this.state.searchKeyword.toLowerCase()) ?
+                      <tr key={index}>
+                        <td>{index + 1}</td>
+                        <td>{education.syear}</td>
+                        <td>{education.eyear}</td>
+                        <td>{education.iname}</td>
+                        <td>{education.cname}</td>
+                        <td className='text-center'>
+                          <NavLink exact to={'/education/edit/' + education.id} className='mr-3'><FaPenSquare className='text-warning' /></NavLink>
+                          <NavLink exact to='#' className='mr-3'><FaTrash className='text-danger' onClick={() => this.onDeleteHandler(education.id)} /></NavLink>
+                        </td>
+                      </tr> : ''
+                  )
+                }
+              </tbody>
+            </Table>
+          </Card.Body>
+        </Card>
+
+        </div>)};
 };
 
 export default Education;
