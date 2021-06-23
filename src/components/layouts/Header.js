0547aa15c82634c10a3513ab7e94b945491cfd83
Prabhjot Sidhu <pavisidhu9@gmail.com>
2021-06-23 12:49:31 -0400

    fixed header and scroll bar bug

--- src/components/layouts/Header.js
+++ src/components/layouts/Header.js
@@ -1,27 +1,28 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
-
+const loggedin = false;
 function Header() {
 
     const logout = () => {
-        
+        alert("User logged out!!")
     }
 
     return (
-        <nav className="navbar navbar-dark sticky-top bg-light flex-md-nowrap p-0 shadow">
-            <a className="navbar-brand col-md-3 col-lg-2 mr-0 px-3" href="/">Resume Builder</a>
-            <button className="navbar-toggler position-absolute d-md-none collapsed" type="button" data-toggle="collapse" data-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-            </button>
-            <ul className="navbar-nav px-3">
-                <li className="nav-item text-nowrap">
-                    
-                        (<NavLink exact to='/login' className='nav-link'>Login</NavLink>)
-                        : (<NavLink exact to='/login' onClick={logout} className='nav-link'>Logout</NavLink>)
-                </li>
-            </ul>
-        </nav>
+        <div className="main-header">
+            <nav className="navbar navbar-dark sticky-top bg-light flex-md-nowrap p-0 shadow">
+                <a className="navbar-brand col-md-3 col-lg-2 mr-0 px-3" href="/">Resume Builder</a>
+                <button className="navbar-toggler position-absolute d-md-none collapsed" type="button" data-toggle="collapse" data-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <ul className="navbar-nav px-3">
+                    <li className="nav-item text-nowrap">
+                            { !loggedin && <NavLink exact to='/login' className='nav-link'>Login</NavLink>}
+                            { loggedin &&<NavLink exact to='/login' onClick={logout} className='nav-link'>Logout</NavLink>}
+                    </li>
+                </ul>
+            </nav>
+        </div>
     );
 }
 
