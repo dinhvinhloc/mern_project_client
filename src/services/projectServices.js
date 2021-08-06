3155355848ac5157ad1a1dc20bea728f9dbb795d
dinhvinhloc <39650247+dinhvinhloc@users.noreply.github.com>
2021-08-06 02:48:09 -0400

    services for Project page

---
+++ src/services/projectServices.js
@@ -0,0 +1,29 @@
+import axios, { HOST } from "../utils/httpUtilities";
+
+const GET_PROJECTS_ENDPOINT = `${HOST}/projects/user`;
+const PROJECT_DETAIL_ENDPOINT = `${HOST}/projects`;
+const ADD_PROJECT_ENDPOINT = `${HOST}/projects`;
+
+export function getAllProjects(payload) {
+    return axios.get(`${GET_PROJECTS_ENDPOINT}/${payload.userId}`);
+}
+
+export function deleteProject(projectId) {
+    return axios.delete(`${PROJECT_DETAIL_ENDPOINT}`, {
+        id: projectId
+    });
+}
+
+export function addProject(payload) {
+    return axios.post(`${ADD_PROJECT_ENDPOINT}`, payload);
+}
+
+export function detailProject(projectId) {
+    return axios.get(`${PROJECT_DETAIL_ENDPOINT}/${projectId}`);
+}
+
+export function updateProject(payload) {
+    return axios.put(`${PROJECT_DETAIL_ENDPOINT}`, payload);
+}
+
+
