cf88993c50c013a97f7c69daee9795a5b75ae027
Vlad Kostur <kostur.vlad@gmail.com>
2021-08-15 20:56:07 -0400

    modified award section: display/add/edit

--- src/components/pages/Award/AddAward.js
+++ src/components/pages/Award/AddAward.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import Breadcrumbs from '../../layouts/Breadcrumbs';
-import { NavLink } from 'react-router-dom';
 import { useState } from 'react';
+import Breadcrumbs from '../../layouts/Breadcrumbs';
 import { Button, Card, Form, Col, Alert } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
+import * as awardServices from './../../../services/awardServices';
+import { useHistory } from 'react-router-dom';
 
 const breadcrumbLinks = [
   {
@@ -21,72 +23,97 @@ const breadcrumbLinks = [
 ];
 
 const AddAward = () => {
-  const [title, setTitle] = useState('');
-  const [description, setDescription] = useState('');
-  const [date, setDate] = useState('');
 
-  const handleChangeTitle = (e) => {
-    setTitle(e.target.value);
-  };
-
-  const handleChangeDescription = (e) => {
-    setDescription(e.target.value);
-  };
-
-  const handleChangeDate = (e) => {
-    setDate(e.target.value);
-  };
+  const history = useHistory();
+  const [payload, setPayload] = useState({
+    title: '',
+    description: '',
+    date: ''
+  })
+  const [error, setError] = useState(
+    {
+      messageVariant: 'danger',
+      message: '',
+    }
+  )
 
   const saveHandler = (e) => {
 
+
+    awardServices.addAward(payload)
+      .then(response => {
+        console.log(response.data);
+        history.push('/award')
+      })
+      .catch((error) => {
+        let errorMessage = []
+        error.errors.forEach(error =>{
+          errorMessage.push(error.param + ": " + error.msg)
+        })
+
+        setError({
+          messageVariant: 'danger',
+          message: errorMessage.join(),
+        });
+      });
   }
-  return <div>
-    <Breadcrumbs links={breadcrumbLinks} />
-    <Card
-          bg='light'
-          text='dark'
-        >
-        <Card.Header>Add Award or Achievment</Card.Header>
-        <Card.Body>
-          <Form>
-            <Form.Group>
-              <Form.Row>
-                <Col>
-                  <Form.Label>Award Title:</Form.Label>
-                  <Form.Control
-                  size='sm' type='text' name='title'
-                  placeholder='Enter your award title'
-                  onChange={handleChangeTitle} />
-                </Col>
-                <Col>
-                  <Form.Label>Description:</Form.Label>
-                  <Form.Control
-                  size='sm' type='text' name='description'
-                  placeholder='Shortly describe it'
-                  onChange={handleChangeDescription} />
-                </Col>
-                <Col>
-                  <Form.Label>Date obtained:</Form.Label>
-                  <Form.Control
-                  size='sm' type='text' name='date'
-                  placeholder='MM/YEAR'
-                  onChange={handleChangeDate} />
-                </Col>
-              </Form.Row>
-            </Form.Group>
-          </Form>
-        </Card.Body>
-        <Card.Footer>
-          <Button size='sm' variant='success' className='float-right'
-            type='submit'
-            onClick={saveHandler}>
-              Save
-          </Button>
-          <NavLink exact to='/award' className='btn btn-sm btn-outline-secondary float-left'>Back to Awards and Achievments Overview
-          </NavLink>
-        </Card.Footer>
-      </Card>
-  </div>;
+
+  const handleValueChange = (e) => {
+    setPayload({...payload, [e.target.name]: e.target.value });
+  };
+
+  return (
+    <div>
+        <Breadcrumbs links={breadcrumbLinks} />
+        {
+          error.message ? <Alert variant={error.messageVariant}>{error.message}</Alert> : ''
+        }
+        <Card
+            bg='light'
+            text='dark'
+          >
+          <Card.Header>Add Award or Achievment</Card.Header>
+          <Card.Body>
+            <Form>
+              <Form.Group>
+                <Form.Row>
+                  <Col>
+                    <Form.Label>Award Title:</Form.Label>
+                    <Form.Control
+                    size='sm' type='text' name='title'
+                    placeholder='Enter your award title'
+                    onChange={handleValueChange} />
+                  </Col>
+                  <Col>
+                    <Form.Label>Description:</Form.Label>
+                    <Form.Control
+                    size='sm' type='text' name='description'
+                    placeholder='Shortly describe it'
+                    onChange={handleValueChange} />
+                  </Col>
+                  <Col>
+                    <Form.Label>Date obtained:</Form.Label>
+                    <Form.Control
+                    size='sm' type='text' name='date'
+                    placeholder='MM/YEAR'
+                    onChange={handleValueChange} />
+                  </Col>
+                </Form.Row>
+              </Form.Group>
+            </Form>
+          </Card.Body>
+          <Card.Footer>
+            <Button size='sm' variant='success' className='float-right'
+              type='submit'
+              onClick={saveHandler}>
+                Save
+            </Button>
+            <NavLink exact to='/award' className='btn btn-sm btn-outline-secondary float-left'>Back to Awards and Achievments Overview
+            </NavLink>
+          </Card.Footer>
+        </Card>
+    </div>
+  );
 };
 
 export default AddAward;
--- src/components/pages/Award/EditAward.js
+++ src/components/pages/Award/EditAward.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 import Breadcrumbs from '../../layouts/Breadcrumbs';
-import { NavLink } from 'react-router-dom';
-import { useState } from 'react';
 import { Button, Card, Form, Col, Alert } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
+import * as awardServices from './../../../services/awardServices';
+import { useHistory } from 'react-router-dom';
 
 const breadcrumbLinks = [
   {
@@ -20,75 +22,124 @@ const breadcrumbLinks = [
 ];
 
 const EditAward = () => {
-  const [title, setTitle] = useState('');
-  const [description, setDescription] = useState('');
-  const [date, setDate] = useState('');
+  const history = useHistory();
 
-  const handleChangeTitle = (e) => {
-    setTitle(e.target.value);
-  };
+  const [payload, setPayload] = useState({
+    id: '',
+    title: '',
+    description: '',
+    date: ''
+  })
 
-  const handleChangeDescription = (e) => {
-    setDescription(e.target.value);
-  };
+  const sendGetRequest = async () => {
+    try {
 
-  const handleChangeDate = (e) => {
-    setDate(e.target.value);
+      console.log(payload.id)
+      awardServices.detailAward(props.match.params.id)
+        .then(function (response) {
+          console.log(response.data)
+          setPayload({
+            id: response.data._id, title: response.data.title, description: response.data.description,
+            date: response.data.date
+          });
+        })
+
+    } catch (err) {
+      // Handle Error Here
+      console.error(err);
+    }
   };
+  useEffect(() => {
+    sendGetRequest();
+  }, []);
+
+
+  const [error, setError] = useState(
+    {
+      messageVariant: 'danger',
+      message: '',
+    }
+  )
 
   const saveHandler = (e) => {
 
+    awardServices.updateAward(payload)
+      .then(response => {
+        console.log(response.data);
+        history.push('/award')
+      })
+      .catch((error) => {
+        let errorMessage = []
+        error.errors.forEach(error => {
+          errorMessage.push(error.param + ": " + error.msg)
+        })
+
+        setError({
+          messageVariant: 'danger',
+          message: errorMessage.join(),
+        });
+      });
   }
 
-  return <div>
-  <Breadcrumbs links={breadcrumbLinks} />
-  <Card
-    bg='light'
-    text='dark'
-  >
-    <Card.Header>Edit Award or Achievment details</Card.Header>
-    <Card.Body>
-      <Form>
-        <Form.Group>
-          <Form.Row>
-            <Col>
-              <Form.Label>Award Title:</Form.Label>
-              <Form.Control
-              size='sm' type='email' name='title'
-              placeholder='Enter your award title'
-              value={title}
-              onChange={handleChangeTitle} />
-            </Col>
-            <Col>
-              <Form.Label>Description:</Form.Label>
-              <Form.Control
-                size='sm' type='text' name='description'
-                placeholder='Shortly describe it'
-                value={description}
-                onChange={handleChangeDescription} />
-            </Col>
-            <Col>
-              <Form.Label>Date obtained:</Form.Label>
-              <Form.Control
-              size='sm' type='text' name='date'
-              placeholder='MM/YEAR'
-              value={date}
-              onChange={handleChangeDate} />
-            </Col>
-          </Form.Row>
-        </Form.Group>
-      </Form>
-    </Card.Body>
-    <Card.Footer>
-      <Button size='sm' variant='success' className='float-right'
-        type='submit'
-        onClick={saveHandler}>
-          Save
-      </Button>
-      <NavLink exact to='/award' className='btn btn-sm btn-outline-secondary float-left'>Back to Awards and Achievments Overview
-      </NavLink>
-    </Card.Footer>
-  </Card></div>;
+  const handleValueChange = (e) => {
+    setPayload({ ...payload, [e.target.name]: e.target.value });
+  };
+
+  return (
+    <div>
+      <Breadcrumbs links={breadcrumbLinks} />
+      {
+        error.message ? <Alert variant={error.messageVariant}>{error.message}</Alert> : ''
+      }
+      <Card
+        bg='light'
+        text='dark'
+      >
+        <Card.Header>Edit Award or Achievment details</Card.Header>
+        <Card.Body>
+          <Form>
+            <Form.Group>
+              <Form.Row>
+                <Col>
+                  <Form.Label>Award Title:</Form.Label>
+                  <Form.Control
+                  size='sm' type='email' name='title'
+                  placeholder='Enter your award title'
+                  value={payload.title}
+                  onChange={handleValueChange} />
+                </Col>
+                <Col>
+                  <Form.Label>Description:</Form.Label>
+                  <Form.Control
+                    size='sm' type='text' name='description'
+                    placeholder='Shortly describe it'
+                    value={payload.description}
+                    onChange={handleValueChange} />
+                </Col>
+                <Col>
+                  <Form.Label>Date obtained:</Form.Label>
+                  <Form.Control
+                  size='sm' type='text' name='date'
+                  placeholder='MM/YEAR'
+                  value={payload.date}
+                  onChange={handleValueChange} />
+                </Col>
+              </Form.Row>
+            </Form.Group>
+          </Form>
+        </Card.Body>
+        <Card.Footer>
+          <Button size='sm' variant='success' className='float-right'
+            type='submit'
+            onClick={saveHandler}>
+              Save
+          </Button>
+          <NavLink exact to='/award' className='btn btn-sm btn-outline-secondary float-left'>Back to Awards and Achievments Overview
+          </NavLink>
+        </Card.Footer>
+      </Card>
+    </div>
+  );
 };
 
 export default EditAward;
